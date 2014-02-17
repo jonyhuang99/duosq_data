d0422cbc64b7cda6b868f8c1ac265793978ff56f
jon.huang <sakura.speed@gmail.com>
2014-02-18 01:10:50 +0800

    增加时代互联短信发送api

---
+++ api/now.php
@@ -0,0 +1,35 @@
+<?PHP
+//时代互联接口访问底层
+namespace API;
+require_once "now/sms.inc.php";
+
+class Now extends _Api {
+
+	var $client;
+
+	function __construct(){
+		$this->client = new \SMS();
+	}
+
+	function sendSms($mobile, $message){
+
+		$time = time();
+		$apitype = 3; // $apitype 通道选择 0：默认通道； 2：通道2； 3：即时通道；
+		$respxml=$this->client->sendSMS($mobile, g2u($message, true), $time, $apitype);
+
+		$_SESSION["xml"]=$this->client->sendXML;
+		$_SESSION["respxml"]=$respxml;
+		$code=$this->client->getCode();
+		$respArr=$this->client->toArray();
+		$mess=$respArr["msg"];
+		$smsid=$respArr["idmessage"][0];
+		$succnum=$respArr["successnum"][0];
+		$succphone=$respArr["successphone"][0];
+		$failephone=$respArr["failephone"][0];
+		$status=$respArr["smsstatus"][0];
+
+		return $succphone;
+	}
+}
+
+?>
---
+++ api/now/config.inc.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * 这是配置文件  * $vcpserver		SCP服务器地址，测试服务器为testxml.todaynic.com，正式服务器为sms.todaynic.com
+ * $vcpserverport	SCP服务器，在测试环境和真实环境，使用的接口均为20002
+ * $vcpsuser		时代互联提供的真实短信用户或测试用户
+ * $vcppassword		 时代互联提供的真实短信用户密码或测试用户密码
+ *
+ * www.now.com,Inc. http://www.now.com
+**/
+$vcpserver="sms.todaynic.com";
+$vcpserverport="20002";
+$vcpuser="ms97519";
+$vcppassword="mji5od";
+?>
