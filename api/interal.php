5c5e35d333e0ea99fcd1ec64d821500bb1ac5dd0
jon.huang <sakura.speed@gmail.com>
2014-06-15 08:41:03 +0800

    新人集分宝走内部接口打款

---
+++ api/interal.php
@@ -0,0 +1,20 @@
+<?PHP
+//内部接口访问底层
+namespace API;
+
+class Interal extends _Api {
+
+	var $pre = 'http://api.duosq.com/Interal/';
+
+	function pay($user_id){
+
+		if(!$user_id)return ;
+
+		$url = $this->pre . 'pay/' . $user_id;
+		$api_ret = file_get_contents($url);
+		$api_return = json_decode($api_ret, true);
+		return $api_return;
+	}
+}
+
+?>
