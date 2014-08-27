49b827b1b945933d6a075886ac897eb5d2fd7834
jon.huang <sakura.speed@gmail.com>
2014-08-27 18:13:36 +0800

    增加品牌编辑功能

--- db/promotion/brand.php
+++ db/promotion/brand.php
@@ -7,10 +7,15 @@ class Brand extends _Db {
 	var $name = 'brand';
 	var $useDbConfig = 'promotion';
 
+	var $validate = array('cat'=>VALID_NOT_EMPTY);
+
 	//新增品牌，返回品牌ID
 	function add($data){
 
-		if(!$data['name'] && !$data['name_en'])return;
+		if(!@$data['cat']){
+			$this->validationErrors['cat'] = 1;
+			return;
+		}
 		return parent::add($data);
 	}
 }
